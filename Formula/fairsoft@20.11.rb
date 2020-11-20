eb07ddb20d86db3fefaf80a4e7b06cc5047d2589
Dennis Klein <d.klein@gsi.de>
2020-11-20 04:56:32 +0100

    Add new catalina bottle

--- Formula/fairsoft@20.11.rb
+++ Formula/fairsoft@20.11.rb
@@ -10,6 +10,7 @@ class FairsoftAT2011 < Formula
   bottle do
     root_url "https://alfa-ci.gsi.de/packages"
     sha256 "43886e6b194616b76a4fface1ca90a9d4b2ee524af439c9d38487cc3a03a75fe" => :big_sur
+    sha256 "e5828f10ea2fab86b1b11552e6be75dc72ab42b412bbfeeeb53533160e69ac30" => :catalina
   end
 
   pour_bottle? do
