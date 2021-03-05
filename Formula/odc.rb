9cbdb5fc3d33428c997362014dd4302dd1bf42de
Dennis Klein <d.klein@gsi.de>
2021-03-05 07:06:33 +0100

    odc: Add bottles

--- Formula/odc.rb
+++ Formula/odc.rb
@@ -9,6 +9,8 @@ class Odc < Formula
 
   bottle do
     root_url "https://alfa-ci.gsi.de/packages"
+    sha256 big_sur: "77c1ba768f40ec2e2a012f3c82cc3d57484c9e2a505ee7a60f52c473bc539a7f"
+    sha256 catalina: "d8f75700ae40d370cbdc2a3895ee10673ec7f6570325a0968aa7a4f8807657cb"
   end
 
   pour_bottle? do
