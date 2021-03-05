b81c23103a4c02039ca95d736d2079965b7fa2a9
Dennis Klein <d.klein@gsi.de>
2021-03-05 04:42:07 +0100

    dds: Hint icu lib path differently

--- Formula/dds.rb
+++ Formula/dds.rb
@@ -32,12 +32,12 @@ class Dds < Formula
   def install
     inreplace "CMakeLists.txt", "set(DDS_BOOST_LIB_DIR ${Boost_LIBRARY_DIR})",
                                 "set(DDS_BOOST_LIB_DIR ${Boost_LIBRARY_DIRS})"
-    ENV["DDS_LD_LIBRARY_PATH"] = Formula["icu4c"].opt_lib
 
     builddir = "build"
     args = std_cmake_args.reject{ |e| e =~ /CMAKE_(CX*_FLAGS|BUILD_TYPE|VERBOSE_MAKEFILE)/ }
     args << "-GNinja"
     args << "-DCMAKE_BUILD_TYPE=RelWithDebInfo"
+    args << "-DENV_LD_LIBRARY_PATH=#{Formula["icu4c"].opt_lib}"
     system "cmake", "-S", ".", "-B", builddir, *args
     system "cmake", "--build", builddir, "--target", "wn_bin"
     system "cmake", "--build", builddir, "--target", "install"
