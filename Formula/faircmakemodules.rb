67e0bfad258b82c5dde4220e25eec985f73973fe
Dennis Klein <d.klein@gsi.de>
2021-05-24 17:50:35 +0200

    faircmakemodules: Package 0.2.0

--- Formula/faircmakemodules.rb
+++ Formula/faircmakemodules.rb
@@ -3,14 +3,12 @@ class Faircmakemodules < Formula
   homepage "https://github.com/FairRootGroup/FairCMakeModules"
   url "https://github.com/FairRootGroup/FairCMakeModules",
     :using => :git,
-    :revision => "0112745f54a45ce70e3bd770b1084e29192c31c9"
+    :revision => "ceecfbad90c9dbdb876a4562403280a7c63163f3"
   license "LGPL-3.0+"
-  version "0.1.0"
+  version "0.2.0"
 
   bottle do
     root_url "https://alfa-ci.gsi.de/packages/brew"
-    sha256 cellar: :any_skip_relocation, big_sur: "8f1a4612d77341651f98aa225c5a64aa9e8ee04a67dcc0670f1216ce5b6b5157"
-    sha256 cellar: :any_skip_relocation, catalina: "2aa7900b013bea3233cba3148c2c889cddf22ad14c986162a5a914b684e4acf9"
   end
 
   depends_on "cmake" => :build
