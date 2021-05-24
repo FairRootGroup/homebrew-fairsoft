031f95e02e7987238285bdefe858adafef41f643
Dennis Klein <d.klein@gsi.de>
2021-05-24 19:51:51 +0200

    faircmakemodules: Add bottles

--- Formula/faircmakemodules.rb
+++ Formula/faircmakemodules.rb
@@ -9,6 +9,8 @@ class Faircmakemodules < Formula
 
   bottle do
     root_url "https://alfa-ci.gsi.de/packages/brew"
+    sha256 cellar: :any_skip_relocation, big_sur: "7b151289aea75ad8ef39ea0a542e640fbf7c1f589f44883a05853c2adc919314"
+    sha256 cellar: :any_skip_relocation, catalina: "7adfbb9cd1f8922697d9307e70b69d0d89b7b7e183f2d950db4c81b12e9cf5ef"
   end
 
   depends_on "cmake" => :build
