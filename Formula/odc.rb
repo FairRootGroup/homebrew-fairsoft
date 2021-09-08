208b179d6b7c8c8802de07ae88d2efeede631a41
Dennis Klein <d.klein@gsi.de>
2021-09-08 18:44:56 +0200

    odc: Bump v0.40

--- Formula/odc.rb
+++ Formula/odc.rb
@@ -3,25 +3,20 @@ class Odc < Formula
   homepage "http://github.com/FairRootGroup/ODC"
   url "https://github.com/FairRootGroup/ODC",
     :using => :git,
-    :revision => "b3adfb2343e182be4507097a58f21f998a551e52"
+    :revision => "f48c2e280e19a79810665cb1b681d3952f714d54"
   license "LGPL-3.0+"
-  version "0.16"
+  version "0.40"
 
-  bottle do
-    root_url "https://alfa-ci.gsi.de/packages"
-    sha256 big_sur: "ed90233471a0e19861472a94ff30b39de3eb8e63f58884c2666b06b6e29b1916"
-    sha256 catalina: "855006fca79385b2872d7d40f804d2acf902771353e6c93b3826f0944e8166ae"
-  end
+  # bottle do
+    # root_url "https://alfa-ci.gsi.de/packages"
+    # sha256 big_sur: "ed90233471a0e19861472a94ff30b39de3eb8e63f58884c2666b06b6e29b1916"
+    # sha256 catalina: "855006fca79385b2872d7d40f804d2acf902771353e6c93b3826f0944e8166ae"
+  # end
 
   pour_bottle? do
     reason "The bottle requires CommandLineTools for Xcode 12+."
     satisfy do
-      recent_clt = false
-      case MacOS.version
-        when "11.0" then recent_clt = MacOS::Xcode.version.major_minor >= ::Version.new("12")
-        when "10.15" then recent_clt = MacOS::Xcode.version.major_minor >= ::Version.new("12")
-      end
-      MacOS::CLT.installed? && recent_clt
+      MacOS::CLT.installed?
     end
   end
 
