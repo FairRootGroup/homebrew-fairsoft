c9ed62e5a9c9f611ceefc9f2e0f941a98cf9a231
Dennis Klein <d.klein@gsi.de>
2021-09-08 17:44:49 +0200

    faircmakemodules: Bump v1.0.0

--- Formula/faircmakemodules.rb
+++ Formula/faircmakemodules.rb
@@ -3,14 +3,14 @@ class Faircmakemodules < Formula
   homepage "https://github.com/FairRootGroup/FairCMakeModules"
   url "https://github.com/FairRootGroup/FairCMakeModules",
     :using => :git,
-    :revision => "ceecfbad90c9dbdb876a4562403280a7c63163f3"
+    :revision => "b1a416dc30fa8d83535e159c98764408c3f89d28"
   license "LGPL-3.0+"
-  version "0.2.0"
+  version "1.0.0"
 
   bottle do
     root_url "https://alfa-ci.gsi.de/packages/brew"
-    sha256 cellar: :any_skip_relocation, big_sur: "7b151289aea75ad8ef39ea0a542e640fbf7c1f589f44883a05853c2adc919314"
-    sha256 cellar: :any_skip_relocation, catalina: "7adfbb9cd1f8922697d9307e70b69d0d89b7b7e183f2d950db4c81b12e9cf5ef"
+    #sha256 cellar: :any_skip_relocation, big_sur: "7b151289aea75ad8ef39ea0a542e640fbf7c1f589f44883a05853c2adc919314"
+    #sha256 cellar: :any_skip_relocation, catalina: "7adfbb9cd1f8922697d9307e70b69d0d89b7b7e183f2d950db4c81b12e9cf5ef"
   end
 
   depends_on "cmake" => :build
