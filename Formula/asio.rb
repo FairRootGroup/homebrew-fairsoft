2a75b4cf3d691e5e76feef7e7be517f5faf0dbfc
Dennis Klein <d.klein@gsi.de>
2021-10-01 15:49:14 +0200

    asio: Add arm64_big_sur bottle

--- Formula/asio.rb
+++ Formula/asio.rb
@@ -11,6 +11,7 @@ class Asio < Formula
     root_url "https://alfa-ci.gsi.de/packages/brew"
     sha256 cellar: :any_skip_relocation, catalina: "762f8166c7fdb52a00b04adb261d557085ce97dfd13d36aaace907793ffe23a1"
     sha256 cellar: :any_skip_relocation, big_sur: "3b5624c3443505d4b01d77e3b6db7e9391dd491386139eb78bb872c4656f9f03"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "e2e1ac98aa2473713d1c25fa3cc9f89b294bc6d7be90e91305b73fba1ecba1bb"
   end
 
   depends_on "cmake" => :build
