776db31a61328203d57bc70a933be21ea58f3eda
Dennis Klein <d.klein@gsi.de>
2022-01-19 14:04:08 +0100

    odc: Update bottle url

--- Formula/odc.rb
+++ Formula/odc.rb
@@ -8,7 +8,7 @@ class Odc < Formula
   version "0.62"
 
   bottle do
-    root_url "https://alfa-ci.gsi.de/packages"
+    root_url "https://alfa-ci.gsi.de/packages/brew"
     sha256 big_sur: "b0b1499375c2905ea4b749de6d3081fe28ea5a2e8d828527e41e7a5080a681a0"
     sha256 catalina: "5886eb0b75ab7b94c8bef3115bedea7d95ead18abc80cc93c3c990569f8dbc40"
     sha256 arm64_monterey: "ae6b631438be5f84a8d459283610e1b01e30a0849387d05fa4ae1fe967b8737a"
